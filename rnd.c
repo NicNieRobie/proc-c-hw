a4be46f852c2ff31cab4d95d4df7edcbf183c6b0
Vladislav Pendishchuk <vmpendischuk@edu.hse.ru>
2021-09-29 15:40:38 +0300

    Changed folder hierarchy

--- rnd.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <time.h>
-
-int RandInt(int lowerLimit, int upperLimit) {
-    return (rand() % (upperLimit - lowerLimit + 1)) + lowerLimit;
-}
--- rnd.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROC_C_1_RND_H_
-#define PROC_C_1_RND_H_
-
-int RandInt(int lowerLimit, int upperLimit);
-
-#endif //PROC_C_1_RND_H_
